5a09320444d92dc4abdf157c7c29b5808e046cb6
huanrusong <huan.rusong@immomo.com>
2020-04-29 10:41:55 +0800

    整理下中台头文件的宏

--- Frameworks/MLStreamPublisher.framework/Headers/MomoMediaPlayer.h
+++ Frameworks/MLStreamPublisher.framework/Headers/MomoMediaPlayer.h
@@ -5,8 +5,8 @@
 //  Created by hrs on 2019/8/29.
 //
 
-#ifndef MomoMediaEngine_h
-#define MomoMediaEngine_h
+#ifndef MomoMediaPlayer_h
+#define MomoMediaPlayer_h
 #import <Foundation/Foundation.h>
 @class MomoMediaPlayer;
 @protocol MomoMediaPlayerDelegate <NSObject>
@@ -72,4 +72,4 @@
 - (void)updatePlayerViewWithFrame:(CGRect)frame;
 @end
 
-#endif /* MomoMediaEngine_h */
+#endif /* MomoMediaPlayer_h */
