1594c0fe97d76ac9c8580abe342eb4b4a7b7c7c5
li.hao <li.hao@immomo.com>
2021-06-03 14:33:05 +0800

    添加MLStreamPublisher.h

---
+++ Frameworks/MLStreamPublisher.framework/Headers/MLStreamPublisher.h
@@ -0,0 +1,32 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "MLAudioSource.h"
+#import "MLDomainAnalysis.h"
+#import "MLMediaAPI.h"
+#import "MLPublisherCameraConfiguration.h"
+#import "MLStreamConfiguration.h"
+#import "MLStreamMediaSource.h"
+#import "MMLiveContext.h"
+#import "MMCommonParam.h"
+#import "MMLiveEngine.h"
+#import "MMLivePlayer.h"
+#import "MMLiveRTC.h"
+#import "MMLiveRTMPPush.h"
+#import "MMLiveSource.h"
+#import "MomoMediaContext.h"
+#import "MomoMediaPlayer.h"
+#import "MomoMediaPublisher.h"
+
+FOUNDATION_EXPORT double MLStreamPublisherVersionNumber;
+FOUNDATION_EXPORT const unsigned char MLStreamPublisherVersionString[];
+
