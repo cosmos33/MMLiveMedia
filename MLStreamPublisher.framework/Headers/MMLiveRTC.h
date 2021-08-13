3eadd7edbf117017a4e086f1070c6a72c320e7fd
huanrusong <huan.rusong@immomo.com>
2021-08-13 12:08:27 +0800

    支持腾讯和声网高音质

---
+++ MLStreamPublisher.framework/Headers/MMLiveRTC.h
@@ -0,0 +1,360 @@
+//
+//  MMLiveRTC.h
+//  MLStreamPublisher
+//
+//  Created by hrs on 2020/2/6.
+//
+
+#ifndef MMLiveRTC_h
+#define MMLiveRTC_h
+#import <Foundation/Foundation.h>
+#import "MMCommonParam.h"
+#import <CoreMedia/CoreMedia.h>
+
+@class MMLiveRTC;
+#pragma mark - MMLiveRTCDelegate
+@protocol MMLiveRTCDelegate <NSObject>
+
+/**
+推流器开始推流
+*/
+- (void)MMLiveRTCPushStarting:(MMLiveRTC *)pusher type:(MMLivePushType)type error:(NSError*)error;
+
+/**
+推流器停止推流
+*/
+- (void)MMLiveRTCPushStopped:(MMLiveRTC *)pusher type:(MMLivePushType)type error:(NSError*)error;
+
+/**
+推流器推流失败
+*/
+- (void)MMLiveRTCPushFailed:(MMLiveRTC *)pusher type:(MMLivePushType)type error:(NSError*)error;
+
+/**
+推流卡顿开始
+*/
+- (void)MMLiveRTCBufferStart:(MMLiveRTC *)pusher type:(MMLivePushType)type error:(NSError*)error;
+
+/**
+推流卡顿结束
+*/
+- (void)MMLiveRTCBufferStopped:(MMLiveRTC *)pusher type:(MMLivePushType)type error:(NSError*)error;
+
+/**
+推流数据到达cdn（经验值），只用于平滑切换的优化方案
+*/
+- (void)MMLiveRTCPushReplaced:(MMLiveRTC *)pusher type:(MMLivePushType)type error:(NSError*)error;
+
+/**
+用户自己加入频道
+*/
+- (void)MMLiveRTC:(MMLiveRTC *)pusher hostDidJoinChannel:(NSString *)channel type:(MMLivePushType)type;
+
+/**
+其他用户加入频道
+*/
+- (void)MMLiveRTC:(MMLiveRTC *)pusher didMemberJoinChannel:(NSString *)channel withUid:(NSUInteger)uid type:(MMLivePushType)type;
+
+/**
+其他用户退出频道
+*/
+- (void)MMLiveRTC:(MMLiveRTC *)pusher MemberLeaveWithUserId:(NSString *)userId reason:(NSInteger)reason type:(MMLivePushType)type;
+
+/**
+用户离开频道
+*/
+- (void)MMLiveRTChostDidLeaveChannel:(MMLiveRTC *)pusher type:(MMLivePushType)type;
+
+/**
+其他用户掉线
+*/
+- (void)MMLiveRTC:(MMLiveRTC *)pusher didMemberOfflineWithUid:(NSString *)uid type:(MMLivePushType)type reason:(MMLiveRTCUserOffline)reason;
+
+/**
+收到其他用户第一帧视频
+*/
+- (void)MMLiveRTC:(MMLiveRTC *)pusher didReceivedFirstVideoFrameWithRemoteId:(NSString *)uid remoteView:(UIView *)remoteView type:(MMLivePushType)type;
+
+/**
+其他用户声音是否静音
+*/
+- (void)MMLiveRTC:(MMLiveRTC *)pusher didMemberAudioMuted:(BOOL)muted remoteUid:(NSUInteger)uid type:(MMLivePushType)type;
+
+/**
+其他用户画面是否关闭
+*/
+- (void)MMLiveRTC:(MMLiveRTC *)pusher didMemberVideoMuted:(BOOL)muted remoteUid:(NSUInteger)uid type:(MMLivePushType)type;
+
+/**
+连线错误
+*/
+- (void)MMLiveRTC:(MMLiveRTC *)pusher didOccurError:(RTCErrorCode)errorCode type:(MMLivePushType)type;
+
+
+- (void)MMLiveRTC:(MMLiveRTC *)pusher onConferenceAudioPacket:(NSData *)data audioInfo:(NSDictionary *)audioInfo type:(MMLivePushType)type;
+
+/**
+音乐播放失败
+*/
+- (void)MMLiveRTCMusicPlayFailed:(MMLiveRTC*)pusher type:(MMLivePushType)type error:(NSError*)error;
+
+/**
+音乐播放完成
+*/
+- (void)MMLiveRTCMusicPlayCompleted:(MMLiveRTC*)pusher type:(MMLivePushType)type error:(NSError*)error;
+
+/**
+推流码率和分辨率变化
+*/
+- (BOOL)MMLiveRTCLevelChange:(MMLiveRTC*)pusher encodeSize:(CGSize)encodeSize videoBitrate:(int)videoBitrate type:(MMLivePushType)type;
+
+/**
+伴奏播放失败
+*/
+- (void)MMLiveRTCEffectPlayFailed:(MMLiveRTC*)pusher effectId:(int)effectId type:(MMLivePushType)type error:(NSError*)error;
+
+/**
+伴奏播放完成
+*/
+- (void)MMLiveRTCPushEffectPlayCompleted:(MMLiveRTC*)pusher effectId:(int)effectId type:(MMLivePushType)type error:(NSError*)error;
+
+/**
+推流地址变化
+*/
+- (void)MMLiveRTCDidChangeStreamUrl:(MMLiveRTC*)pusher type:(MMLivePushType)type;
+
+/**
+频道key过期需要更新key
+*/
+- (void)MMLiveRTCRequestChannelKey:(MMLiveRTC*)pusher type:(MMLivePushType)type;
+
+/**
+频道key将要过期需要新key
+*/
+- (void)MMLiveRTCRequestChannelKeyWillExpire:(MMLiveRTC*)pusher type:(MMLivePushType)type;
+
+/**
+连线用户声波大小
+*/
+- (void)MMLiveRTC:(MMLiveRTC*)pusher reportAudioVolumeIndicationOfSpeakers:(NSDictionary *)volumes type:(MMLivePushType)type;
+
+/**
+udp下行观众的sei
+*/
+- (void)MMLiveRTC:(MMLiveRTC*)pusher didReceiveSEI:(NSString *)sei type:(MMLivePushType)type;
+
+/**
+获取采集大小
+*/
+- (CGSize)MMLiveRTCGetCaptureSize:(MMLiveRTC*)pusher type:(MMLivePushType)type;
+
+// 获取源数据
+- (CVPixelBufferRef)MMLiveRTC:(MMLiveRTC *)publisher rawData:(CVPixelBufferRef)rawdata type:(MMLivePushType)type;
+@end
+
+@protocol MLStreamMediaSource;
+@interface MMLiveRTC: NSObject
+
+- (instancetype)initWithUserConfig:(MMLiveUserConfig *)userConfig;
+/**
+设置代理
+*/
+@property (nonatomic, weak) id<MMLiveRTCDelegate> delegate;
+
+/**
+获取MMLivePusherConfig
+*/
+@property (nonatomic, readonly, strong) MMLiveMediaConfig *pusherConfig;
+
+@property (nonatomic, strong) MMLiveUserConfig *userConfig;
+/**
+* MMLiveSource的输出结点
+* 把MMLiveRTC 的sourceHandle 添加 到MMLiveSource，才可以进行预览和推流
+*/
+@property (nonatomic, strong) id sourceHandle;
+
+/**
+* 用户传入自己图像
+*
+* @param sampleBuffer 传入的图像
+* @return YES 代表传入成功， NO 代表传入失败
+*/
+- (BOOL)pushExternalVideoSampleBuffer:(CMSampleBufferRef)sampleBuffer;
+
+- (void)mediaSource:(id<MLStreamMediaSource>)source didOutputVideoSampleBuffer:(CMSampleBufferRef)sampleBuffer;
+
+/**
+设置用户角色
+@param role 角色类型
+*/
+- (void)setClientRole:(MMLiveRTCClientRole)role;
+
+/**
+设置进房参数
+@param config 配置结构体
+*/
+- (void)setRoomParams:(MMLiveRoomParams *)config;
+
+/**
+更新channelKey
+@param channelKey 动态key
+*/
+- (void)updateChannelKey:(NSString *)channelKey;
+
+/**
+进房
+@param config 进房信息
+*/
+- (int)enterRoom:(MMLiveMediaConfig *)config transcoding:(MMLiveTranscoding*)transcoding;
+
+/**
+退房
+*/
+- (void)leaveRoom;
+
+/**
+设置SEI
+@param transcoding 转推布局
+*/
+- (int)setLiveTranscoding:(MMLiveTranscoding *)transcoding;
+
+/**
+视频推流开关
+
+@param enable YES 打开 NO 关闭
+*/
+- (void)setEnableVideo:(BOOL)enable;
+
+/**
+音频推流开关
+
+@param enable YES 打开 NO 关闭
+*/
+- (void)setEnableAudio:(BOOL)enable;
+
+/**
+是否发送本地音频
+@param audioMute 本地音频发送开关
+*/
+- (void)setLocalAudioMute:(BOOL)audioMute;
+
+/**
+是否发送本地视频
+@param videoMute 本地视频发送开关
+*/
+- (void)setLocalVideoMute:(BOOL)videoMute;
+
+/**
+是否禁止所有的远端音频
+@param audioMute 静音开关
+*/
+- (void)setAllRemoteAudioMute:(BOOL)audioMute;
+
+/**
+是否禁止所有的远端视频
+@param videoMute 远端视频禁用开关
+*/
+- (void)setAllRemoteVideoMute:(BOOL)videoMute;
+
+/**
+禁止远端某个uid音频
+@param mute 静音开关
+@param userId 用户id
+*/
+- (void)setRemoteAudioStreamMute:(BOOL)mute forUser:(NSString *)userId;
+
+/**
+禁止远端某个uid视频
+@param mute 视频禁用开关
+@param userId 用户id
+*/
+- (void)setRemoteVideoStreamMute:(BOOL)mute forUser:(NSString *)userId;
+
+/**
+大小流切换开关
+@param mute 切换开关
+@param userId 用户id
+*/
+- (void)setRemoteDualVideoStreamMute:(BOOL)mute forUser:(NSString *)userId;
+
+/**
+发起跨房pk
+@param roomId 房间id
+@param userId 用户id
+*/
+- (void)accrossOtherRoomWithRoomId:(NSString *)roomId userId:(NSString *)userId;
+
+/**
+结束跨房pk
+@param roomid 房间id
+*/
+- (void)unaccrossOtherRoom:(NSString *)roomid;
+
+/**
+使能软回声消除
+@param enable 软回声消除开关
+*/
+- (void)enableSoftAEC:(BOOL)enable;
+
+/**
+使能媒体音频采集
+@param enable 媒体音频采集开关
+*/
+- (void)enableRemoteIO:(BOOL)enable;
+
+/**
+设置连线播放是外放还是听筒
+@param enable 外放开关
+*/
+- (void)setConferencePlaybackSpeakerphone:(BOOL)enable;
+
+/**
+调整远端用户的音量
+@param volumeScale 音量
+@param userId 用户id
+*/
+- (void)adjustRemoteUserVolmue:(float)volumeScale forUser:(NSString *)userId;
+
+/**
+调整混屏的背景图片
+@param url 背景图片地址
+*/
+- (void)updateConferenceBackgroundImage:(NSURL *)url;
+
+/**
+更新混屏画布的尺寸
+@param size 混屏画布尺寸
+*/
+- (void)updateCanvasSize:(CGSize)size;
+
+/**
+打开与web sdk的互通功能
+@param enable 开关
+*/
+- (void)setEnableWebSdkInteroperability:(BOOL)enable;
+
+/**
+重新推流功能
+@param url 推流地址
+*/
+- (void)setRestartWithURL:(NSURL *)url;
+
+/**
+* 打开平滑切换功能，默认关闭
+*
+* @param enable YES 打开 NO 关闭
+*/
+- (void)enableStreamReplace:(BOOL)enable;
+
+//是否开启音量大小回调
+- (void)setAudioVolumeIndication:(BOOL)enable interval:(int)interval;
+
+/**
+* 实时的获取推流的上行码率
+*
+* @return 上行码率,单位kbps (建议获取间隔不要小于2s)
+*/
+- (long) getRealTimePushBitRate;
+@end
+
+#endif /* MMLiveRTC_h */
---
+++ MLStreamPublisher.framework/Headers/MMLiveSource.h
@@ -0,0 +1,470 @@
+//
+//  MMLiveSource.h
+//  MLStreamPublisher
+//
+//  Created by hrs on 2020/2/6.
+//
+
+#ifndef MMLiveSource_h
+#define MMLiveSource_h
+#import <Foundation/Foundation.h>
+#import "MMCommonParam.h"
+#if CAMERA_ON
+#import <MLVideoProcessing/MLCameraSource.h>
+#import <MLContractCV/MLContractCV.h>
+#else
+#import <CoreMedia/CoreMedia.h>
+#endif
+@interface MMLiveSource : NSObject
+
+#pragma mark - 音视频输出的对象
+/**
+* 添加视频输出对象
+*
+* @param outputSource 输出到的实例对象
+*/
+- (void)addVideoOutputSource:(id)outputSource;
+
+/**
+* 去除视频输出对象
+*
+* @param outputSource 输出到的实例对象
+*/
+- (void)removeVideoOutputSource:(id)outputSource;
+
+#pragma mark - 视频输入和预览
+/**
+* 设置使能视频外部采集
+*
+* @param enable YES 打开SDK外部采集， NO SDK自采集
+*/
+- (void)setEnableExternalCapture:(BOOL)enable;
+
+/**
+* 设置预览大小
+*
+* @param width 预览宽
+* @param height 预览高
+*/
+- (void)setPreviewSize:(NSInteger)width height:(NSInteger)height;
+
+/**
+* 选择渲染方式
+*
+* @param renderMode 0 GPU渲染,1 metal 渲染
+*/
+- (void)setRenderMode:(MMLiveRenderMode)renderMode;
+
+/**
+* 选择是否用doki美颜
+*
+* @param enable YES用doki美颜，NO 用老版美颜
+*/
+- (void)setUseDokiBeautyEnable:(BOOL)enable;
+
+- (void)setUseBeautyType:(MLCameraSourceBeautyType)beautyType;
+
+/**
+* 开始预览
+*
+* @param cameraId 0 代表后置摄像头 ，1 代表前置摄像头
+* @param view 预览视图
+*/
+- (void)startPreview:(NSInteger)cameraId view:(UIView*)view;
+
+/**
+* 停止预览
+*
+*/
+- (void)stopPreview;
+
+/**
+* 切换前后摄像头
+*
+*/
+- (int)switchCamera;
+
+/**
+* 用户发送自定义，需要用户定时每帧都输入
+*
+* @param sampleBuffer 用户z采集的视频
+*/
+- (void)sendCustomVideoData:(CMSampleBufferRef)sampleBuffer;
+
+#pragma mark - 语音输入和预览
+
+/**
+* 添加音频输出对象
+*
+* @param outputSource 输出到的实例对象
+*/
+- (void)addAudioOutputSource:(id)outputSource;
+
+/**
+* 去除音频输出对象
+*
+* @param outputSource 输出到的实例对象
+*/
+- (void)removeAudioOutputSource:(id)outputSource;
+
+/**
+* 开始SDK音频采集
+*
+* @param audioSampleRate 采样率
+* @param audioChannels 声道数
+*/
+- (void)startAudioCapture:(NSInteger)audioSampleRate channels:(NSInteger)audioChannels;
+
+/**
+* 开始SDK音频采集
+*
+*/
+- (void)startAudioCapture;
+
+/**
+* 停止SDK音频采集
+*
+*/
+- (void)stopAudioCapture;
+
+/**
+* 开启音频耳返
+*
+* @param enable YES 开启耳返，NO 关闭耳返
+*/
+- (void)setEarPhoneMonitor:(BOOL)enable;
+
+#pragma mark - 视频特效
+
+- (void)setFaceBoundsFeedback:(void (^)(CGRect faceBounds, CGSize bufferSize))completion;
+
+/**
+* 设置相机聚焦点
+*
+* @param focusPoint 输入的点。
+*/
+- (void)setFocusPoint:(CGPoint)focusPoint;
+
+- (void)setExposurePoint:(CGPoint)exposurePoint;
+
+- (void)updateISO:(float)iso;
+
+/**
+* 使能水印功能
+*
+* @param enable YES 打开水印 NO关闭水印。
+*/
+- (void)setWatermarkEnabled:(BOOL)enable;
+
+/**
+* 设置磨皮等级
+*
+* @param level 0～1。
+*/
+- (void)setSmoothSkinLevel:(CGFloat)level;
+
+/**
+* 设置美白等级
+*
+* @param level 0～1。
+*/
+- (void)setWhitenSkinLevel:(CGFloat)level;
+
+/**
+* 设置大眼级别
+*
+* @param level 大眼级别，level 0～1。
+*/
+- (void)setEyeScaleLevel:(CGFloat)level;
+
+/**
+* 设置瘦脸级别
+*
+* @param level 大脸级别，level 0～1。
+*/
+- (void)setFaceScaleLevel:(CGFloat)level;
+
+/**
+* 设置削脸
+*
+* @param level 大脸级别，level -0.5 ~ 0.5。
+*/
+- (void)setFaceWidthLevel:(CGFloat)level;
+
+/**
+* 设置下巴
+*
+* @param level 下巴级别，level -0.5 ~ 0.5。
+*/
+- (void)setChinLengthLevel:(CGFloat)level;
+
+/**
+* 设置鼻子
+*
+* @param level 鼻子大小级别，level -0.5 ~ 0.5。
+*/
+- (void)setNoseSizeLevel:(CGFloat)level;
+
+/**
+* 设置嘴唇厚度
+*
+* @param level 嘴唇厚度级别，level -0.5 ~ 0.5。
+*/
+- (void)setLipThicknessLevel:(CGFloat)level;
+
+/**
+* 设置额头
+*
+* @param level 额头大小级别，level 0.0 ~ 1.0。
+*/
+- (void)setForeheadLevel:(CGFloat)level;
+
+/**
+* 设置鼻托
+*
+* @param level 鼻托大小级别，level 0.0 ~ 1.0。
+*/
+- (void)setNoseLiftLevel:(CGFloat)level;
+
+/**
+* 设置嘴大小
+*
+* @param level 嘴大小级别，level 0.0 ~ 1.0。
+*/
+- (void)setMouthSizeLevel:(CGFloat)level;
+
+/**
+* 设置腮红
+*
+* @param level 腮红级别，level 0.0 ~ 1.0。
+*/
+- (void)setMakeUpBlushLevel:(CGFloat)level;
+
+/**
+* 设置修容
+*
+* @param level 修容级别，level 0.0 ~ 1.0。
+*/
+- (void)setMakeUpFaceShadow:(CGFloat)level;
+
+/**
+* 设置眼影
+*
+* @param level 眼影级别，level 0.0 ~ 1.0。
+*/
+- (void)setMakeUpEyeShadow:(CGFloat)level;
+
+/**
+* 设置指定素材滤镜特效
+*
+* @param filter 滤镜的lookup图片
+* @param identifier 滤镜的唯一标示号
+*/
+#if CAMERA_ON
+- (void)setFilter:(MLFilterDescriptor *)filter withIdentifier:(int)identifier;
+#endif
+
+/**
+* 设置指定素材滤镜特效的强度
+*
+* @param intensityValue 滤镜特效的强度
+*/
+- (void)setFilterintensity:(CGFloat)intensityValue;
+
+/**
+* 添加贴纸
+*
+* @param decoration 贴纸的描述
+* @param identifier 贴纸的唯一标示号
+*/
+#if CAMERA_ON
+- (void)setSticker:(FDKDecoration*)decoration withIdentifier:(NSString *)identifier;
+#endif
+
+/**
+* 去除贴纸
+*
+* @param identifier 贴纸的唯一标示号
+*/
+- (void)removeStickerWithIdentifier:(NSString *)identifier;;
+
+/**
+* 添加手势
+*
+* @param decoration 手势资源描述
+*/
+#if CAMERA_ON
+- (void)addGesture:(NSArray<MLObjectTriggeredDecoration *> *)decoration ;
+#endif
+
+/**
+* 去除手势
+*
+* @param decoration 手势资源描述
+*/
+#if CAMERA_ON
+- (void)removeGesture:(MLObjectTriggeredDecoration*)decoration;
+#endif
+
+/**
+* 添加美颜
+*
+* @param items 资源描述
+*/
+#if CAMERA_ON
+- (void)addBeautyItems:(NSArray <MLContractBeautySourceItem *> *)items;
+#endif
+
+/**
+* 去除美颜
+*
+* @param items 资源描述
+*/
+#if CAMERA_ON
+- (void)removeBeautyItems:(NSArray <MLContractBeautySourceItem *> *)items;
+#endif
+
+/**
+* 更新美颜
+*
+* @param items 手势资源描述
+*/
+#if CAMERA_ON
+- (void)updateBeautyItem:(MLContractBeautySourceItem *)item key:(NSString*)key intensity:(float)intensity;
+#endif
+
+#pragma mark - 语音特效
+
+/**
+* 设置语音EQ
+*
+* @param value -4 - 4(0, disable)
+*/
+- (void)setEqValue:(NSInteger)value;
+
+/**
+* 设置语音声音pitch的参数
+*
+* @param value 说话声音pitch的参数
+*/
+- (void)setPitchValue:(NSInteger)value;
+
+/**
+* 设置语音特效
+*
+* @param value 特效枚举
+*/
+- (void)setAeFilterValue:(MMLiveAEFilter)value;
+
+/**
+* 设置语音降噪
+*
+* @param level 0 - 4(0, disable)
+*/
+- (void)setDenoiseLevel:(NSInteger)level;
+
+#pragma mark - 背景音乐
+
+/**
+* 设置播放背景音乐
+*
+* @param url 音乐地址
+* @param loopback 是否循环 YES 循环 NO 不循环
+* @param repeatCount 循环次数
+*/
+- (void)playMusicWithUrl:(NSString*)url loopback:(BOOL)loopback repeatCount:(int)repeatCount;
+
+/**
+* 停止音乐播放
+*
+*/
+- (void)stopMusic;
+
+/**
+* 暂停音乐播放
+*
+*/
+- (void)pauseMusic;
+
+/**
+* 恢复音乐播放
+*
+*/
+- (void)resumeMusic;
+
+/**
+* seek播放位置
+*
+* @param pos 播放位置 单位ms
+*/
+- (void)seekMusic:(NSInteger)pos;
+
+/**
+* 获取当前播放位置 单位ms
+*
+*/
+- (NSInteger)musicCurrentPosition;
+
+/**
+* 获取播放文件总长度 单位ms
+*
+*/
+- (NSInteger)musicDuration;
+
+/**
+* 设置音乐播放的pitch
+*
+* @param value 设置音频pitch
+*/
+- (void)setMusicPitch:(NSInteger)value;
+
+#pragma mark - 音效
+
+/**
+* 设置播放音效
+*
+* @param effectid 音效id
+* @param url 播放地址
+* @param loopEnable 是否循环播放
+* @param publishEnable 是否推送到远端
+*/
+- (void)playEffect:(NSInteger)effectid url:(NSString*)url loopEnable:(BOOL)loopEnable publishEnable:(BOOL)publishEnable;
+
+/**
+* 停止某个音效的声音
+*
+*@param effectid 音效标识
+*/
+- (void)stopEffect:(NSInteger)effectid;
+
+/**
+* 停止所有音效播放
+*
+*/
+- (void)stopAllEffect;
+
+/**
+* 设置某个音效的声音
+*
+*@param effectid 音效标识
+*@param volume 音量大小
+*/
+- (void)setEffectVolumeWithId:(NSInteger)effectid volume:(CGFloat)volume;
+
+/**
+* 设置所有音效的声音
+*
+*@param volume 音量大小
+*/
+- (void)setEffectsVolume:(CGFloat)volume;
+
+#if CAMERA_ON
+/**
+* 获取视频采集器对象
+*
+*/
+- (MLCameraSource *)getCurrentCameraSource;
+#endif
+@end
+
+#endif /* MMLiveSource_h */
